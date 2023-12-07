c386be09b4ba5a6160f3ea22ceeb36c7ec086ee1
henrytang05 <tq.quanghuy@gmail.com>
2023-12-07 23:08:17 +0700

    Add custom level selection dialog

--- Pages/LevelSelectionPage/LevelSelectionPage.cpp
+++ Pages/LevelSelectionPage/LevelSelectionPage.cpp
@@ -1,5 +1,6 @@
 #include "LevelSelectionPage.h"
 
+#include "Session/Session.h"
 #include "Style/Style.h"
 LevelSelectionPage::LevelSelectionPage(QStackedWidget* parent) {
     setupLevelSelectionPage();
@@ -42,4 +43,61 @@ void LevelSelectionPage::setupLevelSelectionPage() {
     connect(back, &QPushButton::clicked, this, [this]() {
         emit LevelSelectionPage::backClicked();
     });
+}
+void LevelSelectionPage::customLevelSelection() {
+    QList<int> dimensions;
+    bool validInput = false;
+    do {
+        QDialog dialog(this);
+        QFormLayout form(&dialog);
+
+        QList<QLineEdit*> fields;
+        QLineEdit* lineEdit1 = new QLineEdit(&dialog);
+        QLineEdit* lineEdit2 = new QLineEdit(&dialog);
+        QLineEdit* lineEdit3 = new QLineEdit(&dialog);
+        form.addRow("Row", lineEdit1);
+        form.addRow("Column", lineEdit2);
+        form.addRow("Mine Number", lineEdit3);
+
+        fields << lineEdit1 << lineEdit2 << lineEdit3;
+
+        QDialogButtonBox buttonBox(
+            QDialogButtonBox::Ok | QDialogButtonBox::Cancel, Qt::Horizontal, &dialog
+        );
+        form.addRow(&buttonBox);
+        QObject::connect(
+            &buttonBox, &QDialogButtonBox::accepted, &dialog, &QDialog::accept
+        );
+        QObject::connect(
+            &buttonBox, &QDialogButtonBox::rejected, &dialog, &QDialog::reject
+        );
+
+        if (dialog.exec() != QDialog::Accepted) return;
+        dimensions.clear();
+        validInput = true;
+        for (QLineEdit* lineEdit : fields) {
+            bool ok;
+            int value = lineEdit->text().toInt(&ok);
+            if (!ok || value < 1) {
+                QMessageBox::warning(
+                    this, "Invalid input", "Please enter a valid integer greater than 0"
+                );
+                validInput = false;
+                break;
+            }
+            dimensions << value;
+        }
+        if (dimensions[0] * dimensions[1] <= dimensions[2]) {
+            QMessageBox::warning(
+                this, "Invalid input", "Row * Column must be greater than Mine Number"
+            );
+            validInput = false;
+        }
+    } while (!validInput);
+
+    if (validInput) {
+        Session::SetBoardDimension(dimensions[0], dimensions[1]);
+        Session::SetMineNumber(dimensions[2]);
+    }
+    return;
 }
--- Pages/LevelSelectionPage/LevelSelectionPage.h
+++ Pages/LevelSelectionPage/LevelSelectionPage.h
@@ -1,10 +1,13 @@
 #ifndef LEVEL_SELECTION_PAGE_H
 #define LEVEL_SELECTION_PAGE_H
 #include <QBoxLayout>
+#include <QDialogButtonBox>
+#include <QFormLayout>
 #include <QLabel>
+#include <QLineEdit>
+#include <QMessageBox>
 #include <QPushButton>
 #include <QStackedWidget>
-
 
 class LevelSelectionPage : public QWidget {
     Q_OBJECT
@@ -15,5 +18,7 @@ class LevelSelectionPage : public QWidget {
    signals:
     void backClicked();
     void levelSelected(int difficulty);
+   public slots:
+    void customLevelSelection()
 };
 #endif
