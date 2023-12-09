a6f9a99d628a6e78b24ac73b4c5d180a26586a97
henrytang05 <tq.quanghuy@gmail.com>
2023-12-09 23:29:46 +0700

    Refactor game initialization code

--- main.cpp
+++ main.cpp
@@ -7,17 +7,10 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    // MainWindow* Game = new MainWindow(800, 800);
-    // Game->setFixedSize(700, 700);
-    // Game->showFullScreen();
+    MainWindow* Game = new MainWindow(800, 800);
+    Game->setFixedSize(700, 700);
+    Game->showFullScreen();
     // Game->show();
-    Session::GetInstance();
-    Session::SetBoardDimension(10, 10);
-    Session::SetMineNumber(10);
 
-    Session::GetInstance().setupBoard();
-    Session::GetInstance().serialize();
-    Session::GetInstance().ResetInstance();
-    Session::GetInstance().deserialize();
     return app.exec();
 }
